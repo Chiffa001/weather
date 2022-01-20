dc6d68fd5062fb507c96a7b33b1f3d0bb984d21c
Nakonechny Anton <a.nakonechny@sotbit.ru>
2022-01-20 17:24:39 +0300

    feat: add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Normalize } from 'styled-normalize';
 import App from './components/App';
 import Global from './components/ui/Global';
-import store from './store';
+import { store, persistor } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Normalize />
-      <Global />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <Normalize />
+        <Global />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const persistor = persistStore(store);
+
+// export default store;
