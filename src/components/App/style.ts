1373e432ee46fb986ffeb594aab9de7f3ecf7699
Anton Nakonechny <nakonechny.anton.js@gmail.com>
2022-01-24 17:59:58 +0300

    feat: add background

--- src/components/App/style.ts
+++ src/components/App/style.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 import Flex from '../ui/Flex';
 
+import background from '../../assets/img/background.jpg';
+
 const Wrapper = styled(Flex)`
     min-height: 100vh;
+    background: center / cover url(${background});
 `;
 
 export default Wrapper;
--- src/components/Weather/style.ts
+++ src/components/Weather/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.article`
     width: 60%;
-    background-color: #dcdfe3;
+    background-color: rgba(162, 164, 168, 0.8);
     padding: 20px;
 `;
 
